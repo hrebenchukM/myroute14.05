49ece5c0b899f3ab469a601d4b13356111b13b66
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-05-15 07:01:48 +0300

    ..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {path:"", component:AppComponent},
+    {path:"contacts", component:ContactsComponent},
+    {path:"registration", component:RegistrationComponent}
+
+
+];//определение массивов маршрутов из компонентов
